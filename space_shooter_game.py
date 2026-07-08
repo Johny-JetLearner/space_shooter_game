d1469b71f171c80033f8e72f4330f565f4a39be3
Johny-JetLearner <giannistempelis00@gmail.com>
2026-07-08 10:57:51 +0300

    Add files via upload

--- space_shooter_game.py
+++ space_shooter_game.py
@@ -1,5 +1,13 @@
 import pygame
 screen=pygame.display.set_mode((800,600))
+space=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\space.png")
+space_1=pygame.transform.scale(space,(800,600))
+screen.blit(space_1,(0,0))
+pygame.display.update()
+red=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\red.png")
+red=pygame.transform.scale(red,(44,77))
+yellow=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\yellow.png")
+yellow=pygame.transform.scale(yellow,(44,77))
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
