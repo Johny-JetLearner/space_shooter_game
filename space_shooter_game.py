7a48626f19315a575b9dcbd3e31f705f0eec8b0b
Johny-JetLearner <giannistempelis00@gmail.com>
2026-07-13 13:03:34 +0300

    Add files via upload

--- space_shooter_game.py
+++ space_shooter_game.py
@@ -8,7 +8,12 @@ red=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game develope
 red=pygame.transform.scale(red,(44,77))
 yellow=pygame.image.load(r"C:\Users\pc\Desktop\SiderAddons\Python\pro game developer\yellow.png")
 yellow=pygame.transform.scale(yellow,(44,77))
+rectangle=pygame.Rect(350,100,100,600)
+def display_images():
+  pygame.draw.rect(screen,"white",rectangle)
+  pygame.display.update()
 while True:
+    display_images()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
